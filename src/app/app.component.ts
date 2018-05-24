15b8bc1006ad3cb72351b9e5188aaa7f992a068a
DESKTOP-RFRH1OC\guitar <nattaya.th@ku.th>
2018-05-24 22:27:56 +0700

    aa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,33 +18,47 @@ import { BScanAgentPage } from '../pages/b-scan-agent/b-scan-agent';
 import { BDetailRoomsPage } from '../pages/b-detail-rooms/b-detail-rooms';
 import { BDetailPickKidsPage } from '../pages/b-detail-pick-kids/b-detail-pick-kids';
 
+
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AngularFireAuth } from 'angularfire2/auth';
+import firebase from 'firebase';
+
+import { DynamoDBService } from '../../core/dynamodb.service';
+
+import * as AWS from 'aws-sdk';
+
 @Component({
   templateUrl: 'app.html',
-  
+
 })
 export class MyApp implements OnInit{
   this_user="";
+
+  facebook = {
+    loggedIn : true
+  };
+
   ngOnInit(){
     this.this_user = "tete";
   }
-  
-  
+
+
   @ViewChild(Nav) nav: Nav;
 
   // rootPage: any = HomePage;
   public rootPage: any = HomePage;
   pages: Array<{title: string, component: any}>;
 
-  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen, private afauth:AngularFireAuth) {
     this.initializeApp();
     // used for an example of ngFor and navigation
     this.pages = [
       // { title: 'List', component: ListPage },
-      { title : 'รายชื่อบุตรหลาน' , component: AListKidsPage },
+      { title : 'List of Kids' , component: AListKidsPage },
     //  { title : 'รายชื่อผู้ปกครอง' , component: AListFamilyPage },
       // { title : 'ประวัติการรับบุตรหลาน' , component: AHistoryKidsPage },
-      { title : 'ตัวแทนผู้ปกครอง' , component: AAgentPage },
-      { title : 'แก้ไขข้อมูลส่วนตัว', component: AProfilePage}
+      { title : 'Sub Parent' , component: AAgentPage },
+      { title : 'Edit Profile', component: AProfilePage}
       // { title : 'รายชื่อห้องเรียน' , component: BHistoryKidsPage },
       // { title : 'ประวัติการรับนักเรียน' , component: BListRoomsPage },
       // { title : 'สแกนตัวแทนผู้ปกครอง' , component: BScanAgentPage },
@@ -54,7 +68,9 @@ export class MyApp implements OnInit{
   }
 
   logout(){
-      this.nav.setRoot(HomePage);
+    console.log("outttttttttttttttttttttt")
+    this.nav.setRoot(HomePage);
+    this.logoutwithfb();
   }
 
   initializeApp() {
@@ -70,6 +86,11 @@ export class MyApp implements OnInit{
     // Reset the content nav to have just this page
     // we wouldn't want the back button to show in this scenario
     this.nav.setRoot(page.component);
+  }
+
+  logoutwithfb() {
+    this.facebook.loggedIn = false;
+    this.afauth.auth.signOut();
   }
 
 }
