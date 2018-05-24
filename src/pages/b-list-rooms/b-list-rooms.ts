e255154cced004af5d15eebf1e865dd274c33864
kanpitcha <kanpitcha.w@ku.th>
2018-05-24 16:50:38 +0700

    faiang

--- src/pages/b-list-rooms/b-list-rooms.ts
+++ src/pages/b-list-rooms/b-list-rooms.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { BDetailRoomsPage } from '../b-detail-rooms/b-detail-rooms';
+import { DynamoDBService } from '../../core/dynamodb.service';
 /**
  * Generated class for the BListRoomsPage page.
  *
@@ -14,8 +15,13 @@ import { BDetailRoomsPage } from '../b-detail-rooms/b-detail-rooms';
   templateUrl: 'b-list-rooms.html',
 })
 export class BListRoomsPage {
-
+  room;
+  class;
+  room_list;
+  id;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.getRoom();
+    
   }
 
   
@@ -26,5 +32,24 @@ export class BListRoomsPage {
   ionViewDidLoad() {
     console.log('ionViewDidLoad BListRoomsPage');
   }
+  async getRoom(){
+    console.log('ID : '+DynamoDBService.getID());
+    this.id = DynamoDBService.getID();
+    var params = {
+      TableName : "TeacherRole",
+      FilterExpression : "teacher_id = :teacher_id",
+      ExpressionAttributeValues : {':teacher_id':"1000000001"}
+    };
+    await DynamoDBService.scan(params).then((data => this.room_list= data[0]));
+    await console.log(this.room_list);
+    await DynamoDBService.setRoom(this.room_list);
+    await this.getClassRoom();
+  }
+  async getClassRoom(){
+     await this.class == DynamoDBService.getClass();
+     await this.room == DynamoDBService.getRoom();
+    await console.log('in list room p. '+this.class+"/"+this.room);
+  }
+
 
 }
